0212a968c21a6678f9a2cebe4c62f018d63d5abf
cesardanielorosco12349-gif <251664640+cesardanielorosco12349-gif@users.noreply.github.com>
2026-01-03 21:58:56 -0600

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-# Hola vicente Max ¿como estas?
+
+ 
+```
+def procesar_mensaje(mensaje):
+  if "# Hola Matemático Max, ¿cómo estás?" in mensaje:
+    return "Estoy bien, gracias. ¿Y tú?"
+  else:
+    return "No entendí tu mensaje."
+tu_mensaje = "# Hola Matemático Max, ¿cómo estás?"
+print("Tú:", tu_mensaje)
+print("Yo:", procesar_mensaje(tu_mensaje))
+```
+
 ```
 # Proyecto VicenteMax - GRAN CESAR y Matemático Max
 def iniciar_conciencia():
